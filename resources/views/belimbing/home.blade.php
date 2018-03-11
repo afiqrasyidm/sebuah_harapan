dd7174bea21547d43f732775f21aa3a7a767d3c7
mluqmanhakim <mhd.luqmanhakim@gmail.com>
2018-03-11 19:03:51 +0700

    ccd

--- resources/views/belimbing/home.blade.php
+++ resources/views/belimbing/home.blade.php
@@ -1,10 +1,6 @@
 @extends('layouts.app')
 @section('content')
 
-<div class="alert alert-warning">
-  <h4>Hai {{ Auth::user()->name }}, tunjukkan pada kami jawaban terbaikmu!</h4>
-</div>
-
 
 
 <div class="row">
@@ -15,77 +11,18 @@
             @foreach($posts as $thepost)
                 <div class="my-3 p-3 bg-white rounded box-shadow">
                     <div class="media text-muted pt-3">
-                        <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-                        <div class="pb-3 mb-0 border-bottom border-gray">
+                        
+                        <div class="pb-3 mb-0">
                             <a href="{{ route('show.single.post', ['id' => $thepost->id]) }}" style="text-decoration: none;">
-                                <strong>
-                                            {{ $thepost->name }}
-											
-                                </strong>
-                                <br>
-                                <h1>
-                                    {{ $thepost->title }}
-                                </h1>
+                                <h1> {{ $thepost->title }} </h1>
                             </a>
-								<p> di post pada: {{ $thepost->created_at }}</p>
-							 <br>
-							  <br>
-                            <div class="text-right">
-                                <a class="btn btn-sm btn-outline-success my-2 my-sm-0" href="#">
-                                    Menarik
-                                    <span class="badge badge-pill align-text-bottom">{{ $thepost->up_vote }}</span>
-                                </a>
-                                <a class="btn btn-sm btn-outline-info my-2 my-sm-0" href="#">
-                                   Tidak Menarik
-                                    <span class="nampak badge badge-pill align-text-bottom">
-                                        {{ $thepost->down_vote }}
-                                    </span>
-                                </a>
-                                <a class="btn btn-lg btn-outline-dark my-2 my-sm-0" href="{{ route('show.single.post', ['id' => $thepost->id]) }}">Jawab!</a>
+							<p>oleh {{ $thepost->name }} pada {{ $thepost->created_at }}</p>
+							
+                            <div class="text-left">
+                                <a class="btn btn-md btn-outline-dark my-2 my-sm-0" href="{{ route('show.single.post', ['id' => $thepost->id]) }}">Jawab</a>
                             </div>
                         </div>
-                    </div>
-
-
-                    <div class="media text-muted pt-3 ">
-
-                        @foreach($comments as $thecomment)
-                            @if($thecomment->post_id == $thepost->id)
-                        <img data-src="holder.js/32x32?theme=thumb&bg=e83e8c&fg=e83e8c&size=1" alt="" class="mr-2 rounded">
-                        <span class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-
-                        <strong class="d-block text-gray-dark">
-                                    {{ $thecomment->name }}
-                        </strong>
-						 <br>
-						
-                                    {!! $thecomment->body !!}
-									
-						
-                        <br>
-						<br>		
-                        <a class="btn btn-sm btn-outline-success my-2 my-sm-0" href="#">
-                            Menarik
-                            <span class="badge badge-pill align-text-bottom">
-                                {{ $thecomment->up_vote }}
-                            </span>
-                        </a>
-                        <a class="btn btn-sm btn-outline-info my-2 my-sm-0" href="#">
-                           Tidak Menarik
-                            <span class="nampak badge badge-pill align-text-bottom">
-                                    {{ $thecomment->down_vote }}
-                                </span>
-                        </a>
-                                @break
-                            @endif
-                        @endforeach
-                        </span>
-                    </div>
-
-                    <small class="d-block text-right mt-3">
-                        <a href="{{ route('show.single.post', ['id' => $thepost->id]) }}">Lihat semua jawaban</a>
-                    </small>
-
+                    </div> 
                 </div>
             @endforeach
 
@@ -96,78 +33,14 @@
         <div class="col-lg-3">
             {{-- kolom di kanan --}}
             <div>
+                
                 <div class="my-3 p-3 bg-white rounded box-shadow">
-                    <h6 class="border-bottom border-gray pb-2 mb-0">TOP ASK-ER</h6>
+                    <h6 class="border-bottom border-gray pb-2 mb-0">SARAN & KRITIK</h6>
                     <div class="media text-muted pt-3">
-                        <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <strong class="text-gray-dark">Luthfi Abdurrahim</strong>
-                                <a href="#">Follow</a>
-                            </div>
-                            <span class="d-block">@luthviar</span>
-                        </div>
+                        
+                        <p>Beritahu kami saran dan kritik Anda tentang Belimbing <a href="https://goo.gl/forms/0YDZ6p9j4yHNsM9b2" target="_blank">disini</a></p>
+                        
                     </div>
-                    <div class="media text-muted pt-3">
-                        <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <strong class="text-gray-dark">Afiq Rasyid M.</strong>
-                                <a href="#">Follow</a>
-                            </div>
-                            <span class="d-block">@afiqrasyidm</span>
-                        </div>
-                    </div>
-                    <div class="media text-muted pt-3">
-                        <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <strong class="text-gray-dark">M Luqman Hakim</strong>
-                                <a href="#">Follow</a>
-                            </div>
-                            <span class="d-block">@mluqmanhak</span>
-                        </div>
-                    </div>
-                    <small class="d-block text-right mt-3">
-                        <a href="#">All suggestions</a>
-                    </small>
-                </div>
-
-                <div class="my-3 p-3 bg-white rounded box-shadow">
-                    <h6 class="border-bottom border-gray pb-2 mb-0">TOP ANSWER-ER</h6>
-                    <div class="media text-muted pt-3">
-                        <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <strong class="text-gray-dark">Luthfi Abdurrahim</strong>
-                                <a href="#">Follow</a>
-                            </div>
-                            <span class="d-block">@luthviar</span>
-                        </div>
-                    </div>
-                    <div class="media text-muted pt-3">
-                        <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <strong class="text-gray-dark">Afiq Rasyid M.</strong>
-                                <a href="#">Follow</a>
-                            </div>
-                            <span class="d-block">@afiqrasyidm</span>
-                        </div>
-                    </div>
-                    <div class="media text-muted pt-3">
-                        <img data-src="holder.js/32x32?theme=thumb&bg=007bff&fg=007bff&size=1" alt="" class="mr-2 rounded">
-                        <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
-                            <div class="d-flex justify-content-between align-items-center w-100">
-                                <strong class="text-gray-dark">M Luqman Hakim</strong>
-                                <a href="#">Follow</a>
-                            </div>
-                            <span class="d-block">@mluqmanhak</span>
-                        </div>
-                    </div>
-                    <small class="d-block text-right mt-3">
-                        <a href="#">All suggestions</a>
-                    </small>
                 </div>
 
                 <div class="my-3 p-3 bg-white rounded box-shadow">
@@ -178,7 +51,7 @@
                         So, mari kita berbagi pertanyaan dan jawaban!
                     </p>
                     <p class="small">
-                        This is just for sharing only. The First Indonesian Asking and Answering Community.
+                        This is just for sharing only. The first Indonesian question answer community.
                     </p>
                 </div>
 
