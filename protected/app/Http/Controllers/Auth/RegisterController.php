50dbc91aba7cd4fb8f57b159c4932a684db7b807
Luthfi Abdurrahim <luthviar.a@gmail.com>
2018-03-13 11:37:06 +0700

    rapih

--- protected/app/Http/Controllers/Auth/RegisterController.php
+++ protected/app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -99,7 +99,7 @@ class RegisterController extends Controller
 	{
 		$user->generateToken();
 
-		return redirect('/home');
+		return redirect('/');
 	}
 	
 }
